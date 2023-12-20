6419a60d0450480d667311e38f96789356926a3f
Jean Molin <jeanmolin16@gmail.com>
2023-12-20 18:50:22 -0300

    Serializers and userviews http configuration

--- backend/api/urls/user_urls.py
+++ backend/api/urls/user_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from api.views import user_views as views
 
-"""
+
 urlpatterns = [
     path('register', views.registerUser, name='register'),
     path('profile/', views.getUserProfile, name='user_profile'),
@@ -10,5 +10,5 @@ urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('delete/<str:pk>/', views.deleteUser, name='deleteUser'),
 ]
-"""
+
 
