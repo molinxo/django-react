0d5aa63789517d6926a63ced831dd6ce446c4a5c
Jean Molin <jeanmolin16@gmail.com>
2023-12-21 23:46:41 -0300

    first react files created

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/products/', include('api.urls.product_urls')),
-    # path('api/users/', include('api.urls.user_urls')),
-    # path('api/orders', include('api.urls.order_urls')),
+    path('api/products/', include('api.urls.product_urls')),
+    path('api/users/', include('api.urls.user_urls')),
+    path('api/orders', include('api.urls.order_urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
